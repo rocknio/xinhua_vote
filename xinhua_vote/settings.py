4ee45bf56ec69814a6c4ffaa3042715c5d5c60d9
rocknio <rocknio@gmail.com>
2016-09-21 17:38:20 +0800

    template,static设置

--- xinhua_vote/settings.py
+++ xinhua_vote/settings.py
@@ -122,7 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR + '/static'
 
+TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(__file__), '../templates').replace('\\', '/'),
+)
 
 LOGGING = {
     'version': 1,
